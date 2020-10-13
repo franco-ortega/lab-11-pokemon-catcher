be74f8b7f07cf5b7428297aa76c88ca0e2adb8cb
Franco Ortega <francopdx@gmail.com>
2020-10-12 18:01:03 -0700

    add randomly displayed images

--- app.js
+++ app.js
@@ -2,9 +2,14 @@
 
 import data from './data.js'
 
+const roundsPlayed = document.querySelector('.rounds-played')
 const radioButtons = document.querySelectorAll('input')
+const images = document.querySelectorAll('label > img');
+const pokemonCaught = 0;
+const pokemonEncountered = 0;
 
-console.log(radioButtons);
+console.log(roundsPlayed, radioButtons, images);
+
 console.group('Radio Buttons')
     console.info(radioButtons[0]);
     console.info(radioButtons[1]); 
@@ -23,7 +28,63 @@ console.log(eachButton);
 
     eachButton.addEventListener('change', (e) => {
 console.log('You clicked it!')
+
+/*
+const pokemonPicker = Math.floor(Math.random() * 14);
+
+console.log(pokemonPicker);
+console.log(data[0]);
+
+let accumulator = 0;
+
+const roundsDisplayed = accumulator + 1;
+
+roundsDisplayed.textContent = roundsDisplayed;
+
+return roundsDisplayed;
+*/
+
     })
+    
+}
+
+
+function getRandomPokemon(dataArray) {
+    const index = Math.floor(Math.random() * dataArray.length);
+
+    return dataArray[index];
+}
+
+
+for (let i = 0; i < images.length; i++) {
+    const eachImage = images[i];
+console.log(eachImage);
+    
+
+// The page will start out by displaying 3 randomly selected, different Pokemon
+
+let pokemonOne = getRandomPokemon(data);
+eachImage.src = pokemonOne.url_image;
+console.log ('Image #1' + eachImage.id);
+
+let pokemonTwo = getRandomPokemon(data);
+while (pokemonTwo.id === pokemonOne.id) {
+    pokemonTwo = getRandomPokemon(data)
+}
+eachImage.src = pokemonTwo.url_image;
+console.log ('Image #2' + eachImage.id);
+
+let pokemonThree = getRandomPokemon(data);
+while (pokemonThree.id === pokemonOne.id || pokemonThree.id === pokemonTwo.id) {
+    pokemonThree = getRandomPokemon(data)
+}
+eachImage.src = pokemonThree.url_image;
+console.log ('Image #3' + eachImage.id);
+
+    //display the Pokemon on the main page
+
+
+
 
 }
 
